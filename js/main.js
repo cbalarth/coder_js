e901e2ff49b56308fe0fa20154d44d38508cd519
Carlos Balart <cbalart96@gmail.com>
2022-10-10 20:42:35 -0300

    orden y comentarios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+// 01 - LISTA DE CANCHAS EXISTENTES
 const listaCanchas = [
     {ID:123, nombre:"LOS CASTAÑOS"},
     {ID:456, nombre:"LOS ABEDULES"},
     {ID:789, nombre:"LOS BOLDOS"},
 ]
 
+// 02 - LISTA DE DIAS Y PRECIOS
 const listaDiasPrecios = [
     {dia:1, nombre:"LUNES", precio:10000},
     {dia:2, nombre:"MARTES", precio:10000},
@@ -12,10 +14,12 @@ const listaDiasPrecios = [
     {dia:5, nombre:"VIERNES", precio:15000},
 ]
 
+// 03 - LISTA DE ARRIENDOS REGISTRADOS
 const listaArriendos = [
     {arrID:0, arrCorreo:"cbalart96@gmail.com", arrNombre:"Carlos Balart", arrCanchaID:"123", arrCanchaNombre:"LOS CASTAÑOS", arrFecha:"Mon Oct 10 2022", arrHorario:"12:00", arrPrecio:10000}
 ];
 
+// 04 - CONSTRUCTOR PARA REGISTRAR UN ARRIENDO
 class arriendo{
     constructor(arrID, arrCorreo, arrNombre, arrCanchaID, arrCanchaNombre, arrFecha, arrHorario, arrPrecio){
         this.arrID = arrID;
@@ -29,11 +33,12 @@ class arriendo{
     }
 }
 
+// 05 - MOSTRAR CANCHAS ORDENADAS ALFABÉTICAMENTE POR NOMBRE
 function mostrarCanchasAZ(){
     listaCanchas.sort((a,b) => {
         if (a.nombre > b.nombre){
             return 1;
-        } else if (a.nombre < b.nombre) {
+        } else if (a.nombre < b.nombre){
             return -1;
         } else {
             return 0;
@@ -42,48 +47,49 @@ function mostrarCanchasAZ(){
     listaCanchas.forEach(cancha => alert(`Cancha "${cancha.nombre}", código ${cancha.ID}.`));
 }
 
+// 06 - SISTEMA DE ARRIENDO
 function Arrendar(){  
-    //SELECCION DE CANCHA
+    // a) SELECCION DE CANCHA
     let varIDCancha = document.forms["Form"]["varA"].value;
-    let validarCancha = listaCanchas.some(cancha => cancha.ID == varIDCancha); //True = La cancha existe.
+    let validarCancha = listaCanchas.some(cancha => cancha.ID == varIDCancha); //True = Se escoge cancha con código existente.
     while(validarCancha == false){
         alert("CÓDIGO DE CANCHA INCORRECTO O NO DISPONIBLE");
         varIDCancha = prompt("Ingrese un código de cancha válido:");
-        validarCancha = listaCanchas.some(cancha => cancha.ID == varIDCancha); //True = La cancha existe.
+        validarCancha = listaCanchas.some(cancha => cancha.ID == varIDCancha); //True = Se escoge cancha con código existente.
     }
     let objetoSeleccionCancha = listaCanchas.find(cancha => cancha.ID == varIDCancha);
     alert(`CANCHA SELECCIONADA: ${objetoSeleccionCancha.nombre}`);
 
-    //SELECCION DE DIA
+    // b) SELECCION DE DIA
     let varFecha = new Date(document.forms["Form"]["varB"].value);
     let diaSemana = varFecha.getDay();
-    let validarDia = listaDiasPrecios.some(i => i.dia == diaSemana); //True = Entre lunes y viernes.
+    let validarDia = listaDiasPrecios.some(i => i.dia == diaSemana); //True = Se escoge dia entre lunes y viernes.
     while(validarDia == false){
         alert("DÍA INCORRECTO O NO DISPONIBLE");
         varFecha = new Date(prompt("Ingrese una fecha válida (aaaa/mm/dd): "));
         diaSemana = varFecha.getDay();
-        validarDia = listaDiasPrecios.some(i => i.dia == diaSemana); //True = Entre lunes y viernes.
+        validarDia = listaDiasPrecios.some(i => i.dia == diaSemana); //True = Se escoge dia entre lunes y viernes.
     }
     let seleccionDia = varFecha.toDateString();
     let objetoSeleccionPrecio = listaDiasPrecios.find(i => i.dia == diaSemana);
     alert(`DIA SELECCIONADO: ${seleccionDia}`);
 
-    //SELECCION DE HORA
+    //c) SELECCION DE HORA
     let varHorario = document.forms["Form"]["varC"].value;
     hora = Number(varHorario[0]+varHorario[1]);
     minutos = Number(varHorario[3]+varHorario[4]);
-    let validarDisponibilidad = listaArriendos.some(i => (i.arrCanchaID == varIDCancha) && (i.arrFecha == seleccionDia) && (i.arrHorario == varHorario)); //False = Hay disponibilidad para esa cancha en ese día y hora.
+    let validarDisponibilidad = listaArriendos.some(i => (i.arrCanchaID == varIDCancha) && (i.arrFecha == seleccionDia) && (i.arrHorario == varHorario)); //False = Existe disponibilidad para esa cancha en ese día y hora.
     while((minutos!=0) || (hora<9) || (hora>22) || (validarDisponibilidad == true)){
         alert("HORARIO INCORRECTO O NO DISPONIBLE");
         varHorario = prompt("Ingrese un horario válido (hh:00):");
         hora = Number(varHorario[0]+varHorario[1]);
         minutos = Number(varHorario[3]+varHorario[4]);
-        validarDisponibilidad = listaArriendos.some(i => (i.arrCanchaID == varIDCancha) && (i.arrFecha == seleccionDia) && (i.arrHorario == varHorario)); //False = Hay disponibilidad para ese día y hora.
+        validarDisponibilidad = listaArriendos.some(i => (i.arrCanchaID == varIDCancha) && (i.arrFecha == seleccionDia) && (i.arrHorario == varHorario)); //False = Existe disponibilidad para esa cancha en ese día y hora.
     }
     let seleccionHorario = varHorario;
     alert(`HORA SELECCIONADA: ${seleccionHorario}`);
 
-    //CONFIRMACIÓN
+    // d) CONFIRMACIÓN
     let varNombre = document.forms["Form"]["varD"].value;
     let varCorreo = document.forms["Form"]["varE"].value;
     const arriendo1 = new arriendo(1, varCorreo, varNombre, objetoSeleccionCancha.ID, objetoSeleccionCancha.nombre, seleccionDia, seleccionHorario, objetoSeleccionPrecio.precio);
