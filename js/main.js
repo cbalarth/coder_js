278e3aa77cc55a22ad4db8d1c6d742f4de76afe4
Carlos Balart <cbalart96@gmail.com>
2022-11-07 18:48:50 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// -----------------------------------------------------
+// 01 - CONSTANTES
+// -----------------------------------------------------
 const form = document.getElementById("form");
 const formCancha = document.getElementById("formCancha");
 const formFecha = document.getElementById("formFecha");
@@ -8,50 +11,57 @@ const formCorreo = document.getElementById("formCorreo");
 const precioLUMAMI = 10000;
 const precioJUVI = 15000;
 
+const DateTime = luxon.DateTime;
+let now = DateTime.fromISO(DateTime.now());
+let fechaHoy = now.toLocaleString(DateTime.DATETIME_MED)
+console.log(fechaHoy);
+
 // -----------------------------------------------------
-// 01 - LISTA DE CANCHAS EXISTENTES
+// 02 - LISTA DE CANCHAS EXISTENTES
 // -----------------------------------------------------
 
-// -> 01.a) CREACIÓN
-let listaCanchas = [
-    {ID:123, nombre:"LOS CASTAÑOS", img: "https://mail.cdn.wpchile.com/assets/img-temp/300x300/img1.jpg"},
-    {ID:456, nombre:"LOS ABEDULES", img: "https://mail.cdn.wpchile.com/assets/img-temp/300x300/img2.jpg"},
-    {ID:789, nombre:"LOS BOLDOS", img: "https://mail.cdn.wpchile.com/assets/img-temp/300x300/img3.jpg"},
-]
-// -> 01.b) ORDENAMIENTO ALFABÉTICO
-listaCanchas.sort((a,b) => {
-    if (a.nombre > b.nombre){
-        return 1;
-    } else if (a.nombre < b.nombre){
-        return -1;
-    } else {
-        return 0;
+// -> 02.a) CREACIÓN
+let listaCanchas = [];
+const crearCanchas = async ()=> {
+    try {
+        let res = await fetch("./data/canchas.json");
+        let data = await res.json();
+        data.forEach(cancha => {
+            listaCanchas.push(cancha);
+        });
+// -> 02.b) ORDENAMIENTO ALFABÉTICO
+        listaCanchas.sort((a,b) => {
+            if (a.nombre > b.nombre){
+                return 1;
+            } else if (a.nombre < b.nombre){
+                return -1;
+            } else {
+                return 0;
+            }
+        })
+// -> 02.c) PUBLICACIÓN HTML, SECCIÓN INFORMATIVA
+        listaCanchas.forEach(cancha => {
+            let contenedorCancha = document.createElement("div");
+            contenedorCancha.className = "cancha"; //Asigno clase para luego trabajar con todos las canchas.
+            contenedorCancha.setAttribute("ID", cancha.ID); //Asigno id para luego trabajar con la cancha específica.
+            contenedorCancha.innerHTML = `
+                <h3>CANCHA "${cancha.nombre}"</h3>
+                <p>Código = ${cancha.ID}</p>
+                <img src="${cancha.img}">
+                `;
+            document.getElementById("seccionCanchas").append(contenedorCancha);
+        })
+// -> 02.d) PUBLICACIÓN HTML, LISTA PARA FORMULARIO
+        listaCanchas.forEach(cancha => {
+            let opcionCancha = document.createElement("option");
+            opcionCancha.innerHTML = `<option>${cancha.nombre}</option>`;
+            document.getElementById("formCancha").append(opcionCancha);
+        })
+    } catch(error) {
+        console.log("ERROR");
     }
-})
-
-// -----------------------------------------------------
-// 02 - PUBLICAR CANCHAS EN HTML
-// -----------------------------------------------------
-
-// -> 02.a) SECCION NFORMATIVA
-listaCanchas.forEach(cancha => {
-    let contenedorCancha = document.createElement("div");
-    contenedorCancha.className = "cancha"; //Asigno clase para luego trabajar con todos las canchas.
-    contenedorCancha.setAttribute("ID", cancha.ID); //Asigno id para luego trabajar con la cancha específica.
-    contenedorCancha.innerHTML = `
-        <h3>CANCHA "${cancha.nombre}"</h3>
-        <p>Código = ${cancha.ID}</p>
-        <img src="${cancha.img}">
-        `;
-    document.getElementById("seccionCanchas").append(contenedorCancha);
-})
-
-// -> 02.b) FORMULARIO
-listaCanchas.forEach(cancha => {
-    let opcionCancha = document.createElement("option");
-    opcionCancha.innerHTML = `<option>${cancha.nombre}</option>`;
-    document.getElementById("formCancha").append(opcionCancha);
-})
+}
+crearCanchas();
 
 // -----------------------------------------------------
 // 03 - LISTA DE DÍAS Y PRECIOS
@@ -87,9 +97,9 @@ const listaGlobalArriendos = [
 ];
 
 // -----------------------------------------------------
-// 06 - LISTA PERSONAL DE ARRIENDOS
+// 06 - LISTA LOCAL DE ARRIENDOS
 // -----------------------------------------------------
-const listaPersonalArriendos = JSON.parse(localStorage.getItem("miagenda")) || [];
+const listaLocalArriendos = JSON.parse(localStorage.getItem("miagenda")) || [];
 
 // -----------------------------------------------------
 // 07 - CONSTRUCTOR PARA REGISTRAR UN ARRIENDO
@@ -113,12 +123,29 @@ class arriendo{
 form.onsubmit = (evento)=> {
     evento.preventDefault();
 
-// -> 08.a) VALIDACIÓN DÍA
+// -> 08.a) VALIDACIÓN FECHA 
+    let hora = Number(formHora.value[0]+formHora.value[1]);
+    let fecha1 = DateTime.fromISO(formFecha.value);
+    let fecha2 = fecha1.set({hour: hora});
+    let fecha3 = fecha2.toLocaleString(DateTime.DATETIME_MED);
+    console.log(fecha3);
+    let validarFecha = fecha3 >= fechaHoy;
+    if (validarFecha == false){
+        Toastify({
+            text: "ERROR 01: FECHA PASADA",
+            duration: 15000,
+            close: false,
+            gravity: "top",
+            position: "right",
+        }).showToast();
+    }
+
+// -> 08.b) VALIDACIÓN DÍA DE LA SEMANA
     let diaSemana = (new Date(formFecha.value).getDay())+1;
     let validarDia = listaDiasPrecios.some(i => i.dia == diaSemana); //True = Se escoge dia entre lunes (1) y viernes (5).
     if (validarDia == false){
         Toastify({
-            text: "ERROR 01: DÍA INCORRECTO",
+            text: "ERROR 02: FIN DE SEMANA",
             duration: 15000,
             close: false,
             gravity: "top",
@@ -126,13 +153,12 @@ form.onsubmit = (evento)=> {
         }).showToast();
     }
 
-// -> 08.b) VALIDACIÓN HORA
-    let hora = Number(formHora.value[0]+formHora.value[1]);
+// -> 08.c) VALIDACIÓN HORA
     let minutos = Number(formHora.value[3]+formHora.value[4]);
     let validarHora = ((minutos==0) && (hora>=9) && (hora<=22)); //True= Se escoge hora cerrada entre 9 y 22.
     if(validarHora == false) {
         Toastify({
-            text: "ERROR 02: HORA INCORRECTA",
+            text: "ERROR 03: HORA NO CERRADA",
             duration: 15000,
             close: false,
             gravity: "top",
@@ -140,11 +166,11 @@ form.onsubmit = (evento)=> {
         }).showToast();
     }
 
-// -> 08.c) VALIDACIÓN OCUPADO
+// -> 08.d) VALIDACIÓN OCUPADO
     let validarOcupado = listaGlobalArriendos.some(i => (i.arrCanchaNombre == formCancha.value) && (i.arrFecha == formFecha.value) && (i.arrHora == formHora.value)); //False = Existe disponibilidad para esa cancha en ese día y hora.
     if(validarOcupado == true) {
         Toastify({
-            text: "ERROR 03: CUPO NO DISPONIBLE",
+            text: "ERROR 04: CUPO NO DISPONIBLE",
             duration: 15000,
             close: false,
             gravity: "top",
@@ -152,13 +178,13 @@ form.onsubmit = (evento)=> {
         }).showToast();     
     }
 
-// -> 08.d) VALIDACIONES FALLIDAS: RESETEA FORMULARIO
-    if ((validarDia == false) || (validarHora ==false) || (validarOcupado == true)) {
+// -> 08.e) VALIDACIONES FALLIDAS: RESETEA FORMULARIO
+    if ((validarFecha == false) || (validarDia == false) || (validarHora ==false) || (validarOcupado == true)) {
         form.reset(); //Limpiar formulario.
     }
 
-// -> 08.e) VALIDACIONES EXITOSAS: GENERA ARRIENDO
-    if ((validarDia == true) && (validarHora ==true) && (validarOcupado == false)) {
+// -> 08.f) VALIDACIONES EXITOSAS: GENERA ARRIENDO
+    if ((validarFecha == true) && (validarDia == true) && (validarHora ==true) && (validarOcupado == false)) {
         
         let listaids = [];
         listaGlobalArriendos.forEach(arriendo => {
@@ -171,8 +197,8 @@ form.onsubmit = (evento)=> {
 
         const nuevoArriendo = new arriendo(resID, formCorreo.value, formNombre.value, resCanchaID, formCancha.value, formFecha.value, formHora.value, resPrecio);
         listaGlobalArriendos.push(nuevoArriendo); //Agregar a lista global.
-        listaPersonalArriendos.push(nuevoArriendo); //Agregar a lista personal (Storage).
-        localStorage.setItem("miagenda", JSON.stringify(listaPersonalArriendos));
+        listaLocalArriendos.push(nuevoArriendo); //Agregar a lista personal (Storage).
+        localStorage.setItem("miagenda", JSON.stringify(listaLocalArriendos));
 
         Swal.fire(
             {
@@ -185,7 +211,7 @@ form.onsubmit = (evento)=> {
             timerProgressBar: true,
             customClass: {popup: 'contSWAL',},
         })
-
+        
         form.reset(); //Limpiar formulario.
     }
 }
